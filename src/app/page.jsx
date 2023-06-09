0d1bb0af08756b2823bdc414a8de5cc3be98c92f
rhodinemma <rhodinemma10@gmail.com>
2023-06-09 10:47:47 +0300

    feat: add content and styling to landing page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,23 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Hero from "../../public/hero.png";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>
+          Better design for your digital products.
+        </h1>
+        <p className={styles.desc}>
+          Turning your idea into reality. We bring together the teams from the
+          global tech industry.
+        </p>
+        <button className={styles.button}>See Our Works</button>
+      </div>
+      <div className={styles.item}>
+        <Image src={Hero} alt="" className={styles.img} />
+      </div>
+    </div>
+  );
 }
