62615cb3003ae679caa1144eeb950dbc8f892646
rhodinemma <rhodinemma10@gmail.com>
2023-06-09 10:24:11 +0300

    feat: add css styling modules

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,8 @@
+"use client";
+
 import Link from "next/link";
 import React from "react";
+import styles from "./page.module.css";
 
 const links = [
   {
@@ -36,14 +39,24 @@ const links = [
 
 const Navbar = () => {
   return (
-    <div>
-      <Link href="/">Mastery</Link>
-      <div>
+    <div className={styles.container}>
+      <Link href="/" className={styles.logo}>
+        Mastery
+      </Link>
+      <div className={styles.links}>
         {links.map((link) => (
-          <Link key={link.id} href={link.url}>
+          <Link key={link.id} href={link.url} className={styles.link}>
             {link.title}
           </Link>
         ))}
+        <button
+          className={styles.logout}
+          onClick={() => {
+            console.log("logged out");
+          }}
+        >
+          Logout
+        </button>
       </div>
     </div>
   );
